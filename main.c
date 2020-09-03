cdcd90e3ebbaa139854cf7275322ac8aa8ac6dfc
sarbar2002 <sxs6666@psu.edu>
2020-09-03 21:32:16 +0000

    comments

--- main.c
+++ main.c
@@ -1,3 +1,12 @@
+// Author Sarthak Singh sxs6666@psu.edu
+// Collabrator Anthony Aguilar axa6168@psu.edu
+// Caollabrator Justin Maines jtm5948@psu.edu
+//Collabrator Maurizio Hazoury  mbh5817@psu.edu
+ 
+
+
+
+
 #include <stdio.h>
 #include <readline/readline.h>
 #include <stdlib.h>
